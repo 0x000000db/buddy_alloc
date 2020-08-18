eaff88160faa3dc7f30924ff2a4d7b5e7b25f14d
0x000000db <1853857+0x000000db@users.noreply.github.com>
2020-08-18 10:46:05 +0100

    Some fixes + visualization

--- buddy_alloc.h
+++ buddy_alloc.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <memory>
 
 struct buddy_default_allocator
@@ -6,10 +8,20 @@ struct buddy_default_allocator
 	void free(void* p) { delete[](uint8_t*)p; }
 };
 
+/*
+Bookkeeping indexes look like this
+|        0       |
+|   1    |   2   |
+| 3 | 4  | 5 | 6 |
+
+Each level contains 2^n blocks, so the total size required is 2^0 + 2^1 + 2^2.. etc or for short 2^(n+1)-1
+
+
+*/
 template<typename allocator>
 class buddy_alloc_core
 {
-	inline size_t next_pot(size_t x)
+	inline size_t next_pot(size_t x) // Next power of two
 	{
 		// Non-fixed size..
 		size_t n = 1;
@@ -20,39 +32,40 @@ class buddy_alloc_core
 		return n;
 	}
 
-	static constexpr uint8_t BLOCK_FREE = 0;
-	static constexpr uint8_t BLOCK_SPLIT = 1;
-	static constexpr uint8_t BLOCK_USED = 2;
-	static constexpr uint8_t BLOCK_ALLOC = 3;
+	static constexpr uint8_t BLOCK_FREE = 0; // A block is free 
+	static constexpr uint8_t BLOCK_SPLIT = 1; // A block has sub-allocated (either from left or right)
+	static constexpr uint8_t BLOCK_USED = 2; // Both left and right children are allocated
+	static constexpr uint8_t BLOCK_ALLOC = 3; // This block was used for an allocation
 
 	buddy_default_allocator _allocator;
 	uint8_t* _memory;
 	uint8_t* _bookkeeping; // 1-byte blocks..
-	size_t _arenaSize;
-	size_t _smallestBlock;
+	size_t _arena_size;
+	size_t _smallest_block;
 	size_t _levels;
 
-	bool in_use(uint8_t u)
+	static inline bool in_use(uint8_t u) // Check if a block is allocated or both children are allocated
 	{
 		return u == BLOCK_USED || u == BLOCK_ALLOC;
 	}
 
-	void* alloc_r(size_t n, size_t index, size_t blockSize, size_t memOffset)
+	void* alloc_r(size_t n, size_t index, size_t block_size, size_t mem_offset) // Recurse tree
 	{
-		if (n == blockSize) 
+		if (n == block_size) // Since n is rounded to the next POT (and rounded to the smallest block)
+							// this condition is guaranteed before recursing out of bound
 		{
-			if (_bookkeeping[index] == BLOCK_FREE) // Take this block
+			if (_bookkeeping[index] == BLOCK_FREE) 
 			{
-				_bookkeeping[index] = BLOCK_ALLOC;
-				return _memory + memOffset;
+				_bookkeeping[index] = BLOCK_ALLOC; // Take this block
+				return _memory + mem_offset;
 			}
-			return nullptr; // Either split or already in use
+			return nullptr; // Block is either split or already in use, move back up the tree
 		}
 
 		if (_bookkeeping[index] == BLOCK_FREE) // Split block
 		{
-			size_t c0 = index * 2 + 1;
-			size_t c1 = index * 2 + 2;
+			size_t c0 = index * 2 + 1; // Next row, left
+			size_t c1 = index * 2 + 2; // Next row, right
 			_bookkeeping[index] = BLOCK_SPLIT;
 			_bookkeeping[c0] = BLOCK_FREE;
 			_bookkeeping[c1] = BLOCK_FREE;
@@ -60,77 +73,77 @@ class buddy_alloc_core
 
 		if (_bookkeeping[index] == BLOCK_SPLIT)
 		{
-			size_t c0 = index * 2 + 1;
-			size_t c1 = index * 2 + 2;
-			size_t childBlock = blockSize >> 1;
-			void* r = alloc_r(n, c0, childBlock, memOffset);
+			size_t c0 = index * 2 + 1; // Next row, left
+			size_t c1 = index * 2 + 2; // Next row, right
+			size_t child_block_size = block_size >> 1;
+			void* r = alloc_r(n, c0, child_block_size, mem_offset); // First try left, if null try right
 			if (r == nullptr)
 			{
-				r = alloc_r(n, c1, childBlock, memOffset + childBlock);
+				r = alloc_r(n, c1, child_block_size, mem_offset + child_block_size); 
 			}
 			if (in_use(_bookkeeping[c0]) && in_use(_bookkeeping[c1]))
 			{
-				_bookkeeping[index] = BLOCK_USED;
+				_bookkeeping[index] = BLOCK_USED; // Signal for future calls there is no free space here
 			}
 			return r;
 		}
 
 		return nullptr;
 	}
-	bool free_r(void* p, size_t index, size_t blockSize, size_t memOffset)
+	bool free_r(void* p, size_t index, size_t block_size, size_t mem_offset)
 	{
-		void* thisBlock = (void*)((size_t)_memory + memOffset);
-		if (p == thisBlock)
+		void* this_block = (void*)((size_t)_memory + mem_offset);
+		if (p == this_block)
 		{
-			if (_bookkeeping[index] == BLOCK_ALLOC) // Found our block
+			if (_bookkeeping[index] == BLOCK_ALLOC) // Must have this flag to be our block, left children share an address
 			{
 				_bookkeeping[index] = BLOCK_FREE;
 				return true;
 			}
 		}
 		// p > thisBlock
-		size_t c0 = index * 2 + 1;
-		size_t c1 = index * 2 + 2;
-		size_t childBlock = blockSize >> 1;
-		size_t d = (size_t)p - (size_t)thisBlock;
+		size_t c0 = index * 2 + 1; // Next row, left side
+		size_t c1 = index * 2 + 2; // Next row, right side
+		size_t child_block_size = block_size >> 1;
+		size_t d = (size_t)p - (size_t)this_block;
 		bool freed = false;
-		if (d < childBlock)
+		if (d < child_block_size)
 		{
-			freed = free_r(p, c0, childBlock, memOffset);
+			freed = free_r(p, c0, child_block_size, mem_offset);
 		}
 		else
 		{
-			freed = free_r(p, c1, childBlock, memOffset + childBlock); // Right side
+			freed = free_r(p, c1, child_block_size, mem_offset + child_block_size); // Right side
 		}
 		if (freed)
 		{
-			if (_bookkeeping[c0] == BLOCK_FREE && _bookkeeping[c1] == BLOCK_FREE) // Join blocks
+			if (_bookkeeping[c0] == BLOCK_FREE && _bookkeeping[c1] == BLOCK_FREE) // If left and right are now both free, mark this block free
 			{
-				_bookkeeping[index] = BLOCK_FREE;
-				return true;
+				_bookkeeping[index] = BLOCK_FREE; // split->free
 			}
+			else // either left or right is still in use, go from used->split
+			{
+				_bookkeeping[index] = BLOCK_SPLIT;
+			}
+			return true;
 		}
 		return false;
 	}
 public:
-	buddy_alloc_core(size_t arenaSize, size_t levels)
+	buddy_alloc_core(size_t arena_size, size_t levels)
 	{
-		arenaSize = next_pot(arenaSize); // Must be POT
+		arena_size = next_pot(arena_size); // Must be POT
 
 		// Init bookkeeping
-		size_t n = 0;
-		for (size_t l = 0; l <= levels; l++)
-		{
-			n += (1 << l); // 2^n
-		}
-		_bookkeeping = (uint8_t*)_allocator.alloc(n);
+		size_t bookkeeping_size = (1 << (levels+1)) - 1;
+		_bookkeeping = (uint8_t*)_allocator.alloc(bookkeeping_size);
 		_bookkeeping[0] = BLOCK_FREE;
-		_smallestBlock = arenaSize >> levels;
+		_smallest_block = arena_size >> levels;
 		_levels = levels;
 
 		// Init arena
-		_arenaSize = arenaSize;
-		_memory = (uint8_t*)_allocator.alloc(arenaSize);
+		_arena_size = arena_size;
+		_memory = (uint8_t*)_allocator.alloc(arena_size);
 	}
 
 	buddy_alloc_core(const buddy_alloc_core&) = delete;
@@ -148,22 +161,25 @@ public:
 
 	void* alloc(size_t n)
 	{
-		n = next_pot(n);
-		if (n < _smallestBlock)
+		n = next_pot(n); // Round up to power of two
+		if (n < _smallest_block)
 		{
-			n = _smallestBlock;
+			n = _smallest_block; // Ensure the block size is one available
 		}
-		if (n <= _arenaSize)
+		if (n <= _arena_size)
 		{
-			return alloc_r(n, 0, _arenaSize, 0);
+			return alloc_r(n, 0, _arena_size, 0);
 		}
 		return nullptr;
 	}
 
 	void free(void* p)
 	{
-		free_r(p, 0, _arenaSize, 0);
+		free_r(p, 0, _arena_size, 0);
 	}
+
+	size_t get_levels() const { return _levels; }
+	const uint8_t* get_tree() const { return _bookkeeping; }
 };
 
 typedef buddy_alloc_core<buddy_default_allocator> buddy_alloc;
--- test.cpp
+++ test.cpp
@@ -1,24 +1,66 @@
 #include <memory>
 #include "buddy_alloc.h"
 
+void dump_r(const uint8_t* bookkeeping, size_t index, size_t width, size_t depth, size_t max_depth)
+{
+	for (size_t i = 0; i < width; i++)
+	{
+		printf(" ");
+	}
+	switch (bookkeeping[index])
+	{
+	case 0: printf("F"); break; // Free
+	case 1: printf("S"); break; // Split
+	case 2: printf("U"); break; // Used
+	case 3: printf("#"); break; // Allocated
+	}
+	bool has_children = (bookkeeping[index] == 1 || bookkeeping[index] == 2);
+	if (depth != max_depth && has_children)
+	{
+		printf("\n");
+		dump_r(bookkeeping, index * 2 + 1, width + 1, depth + 1, max_depth);
+		printf("\n");
+		dump_r(bookkeeping, index * 2 + 2, width + 1, depth + 1, max_depth);
+	}
+}
+void dump(buddy_alloc& alloc)
+{
+	dump_r(alloc.get_tree(), 0, 0, 0, alloc.get_levels());
+	printf("\n\n\n");
+}
+
+void* alloc(buddy_alloc& alloc, size_t len)
+{
+	void* p = alloc.alloc(len);
+	dump(alloc);
+	return p;
+}
+
+void release(buddy_alloc& alloc, void* p)
+{
+	alloc.free(p);
+	dump(alloc);
+}
+
 int main()
 {
 	buddy_alloc ba(128, 5);
-	void* a = ba.alloc(24);
-	void* b = ba.alloc(21);
-	void* c = ba.alloc(19);
-	void* d = ba.alloc(7);
-	void* e = ba.alloc(7);
-	void* f = ba.alloc(7); 
-	void* g = ba.alloc(1); 
-	void* h = ba.alloc(1); 
-	void* j = ba.alloc(1); // j = nullptr;
-	ba.free(h);
-	ba.free(g);
-	ba.free(f);
-	ba.free(e);
-	ba.free(d);
-	ba.free(c);
-	ba.free(b);
-	ba.free(a);
+	dump(ba);
+	void* a = alloc(ba, 24);
+	void* b = alloc(ba, 21);
+	void* c = alloc(ba, 19);
+	void* d = alloc(ba, 7);
+	void* e = alloc(ba, 7);
+	void* f = alloc(ba, 7); 
+	void* g = alloc(ba, 1); 
+	void* h = alloc(ba, 1); 
+	void* j = alloc(ba, 1); // j = nullptr;
+	release(ba, h);
+	release(ba, g);
+	release(ba, f);
+	release(ba, e);
+	release(ba, d);
+	release(ba, c);
+	release(ba, b);
+	release(ba, a);
 }
